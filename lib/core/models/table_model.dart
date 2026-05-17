d2ced51224232b931977219542f81fe889e97318
Caner Aydoğan <caneraydogan5829@hotmail.com>
2026-05-17 19:29:52 +0300

    Masa modeli çoklu rezervasyon ve başlangıç/bitiş saatlerini destekleyecek şekilde güncellendi

--- lib/core/models/table_model.dart
+++ lib/core/models/table_model.dart
@@ -8,7 +8,12 @@ class TableModel {
   final String? currentUserId;
   final String status; // available, reserved, occupied
   final DateTime? reservationTime;
+  final String? nextReservationUserId;
+  final DateTime? nextReservationTime;
+  final List<Map<String, dynamic>> futureReservations;
+  final List<String> futureReservationUserIds;
   final DateTime? breakStartTime;
+  final DateTime? sessionStartTime;
 
   TableModel({
     required this.id,
@@ -18,7 +23,12 @@ class TableModel {
     this.currentUserId,
     required this.status,
     this.reservationTime,
+    this.nextReservationUserId,
+    this.nextReservationTime,
+    this.futureReservations = const [],
+    this.futureReservationUserIds = const [],
     this.breakStartTime,
+    this.sessionStartTime,
   });
 
   factory TableModel.fromMap(Map<String, dynamic> map) {
@@ -32,8 +42,24 @@ class TableModel {
       reservationTime: map['reservationTime'] != null 
           ? (map['reservationTime'] as Timestamp).toDate() 
           : null,
+      nextReservationUserId: map['nextReservationUserId'],
+      nextReservationTime: map['nextReservationTime'] != null 
+          ? (map['nextReservationTime'] as Timestamp).toDate() 
+          : null,
+      futureReservations: (map['futureReservations'] as List<dynamic>?)?.map((e) {
+        final m = e as Map<String, dynamic>;
+        return {
+          'userId': m['userId'] as String,
+          'startTime': (m['startTime'] as Timestamp).toDate(),
+          'endTime': m['endTime'] != null ? (m['endTime'] as Timestamp).toDate() : null, // Support endTime
+        };
+      }).toList() ?? [],
+      futureReservationUserIds: (map['futureReservationUserIds'] as List<dynamic>?)?.cast<String>() ?? [],
       breakStartTime: map['breakStartTime'] != null 
           ? (map['breakStartTime'] as Timestamp).toDate() 
+          : null,
+      sessionStartTime: map['sessionStartTime'] != null 
+          ? (map['sessionStartTime'] as Timestamp).toDate() 
           : null,
     );
   }
@@ -47,8 +73,16 @@ class TableModel {
       'currentUserId': currentUserId,
       'status': status,
       'reservationTime': reservationTime,
+      'nextReservationUserId': nextReservationUserId,
+      'nextReservationTime': nextReservationTime,
+      'futureReservations': futureReservations.map((e) => {
+        'userId': e['userId'],
+        'startTime': Timestamp.fromDate(e['startTime'] as DateTime),
+        'endTime': e['endTime'] != null ? Timestamp.fromDate(e['endTime'] as DateTime) : null,
+      }).toList(),
+      'futureReservationUserIds': futureReservationUserIds,
       'breakStartTime': breakStartTime,
+      'sessionStartTime': sessionStartTime,
     };
   }
 }
-
